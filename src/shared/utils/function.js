220d46648843a7b5b6216499ac64b1336059a242
Carlos Roberto Gomes Junior <carlos.rberto@gmail.com>
2019-03-14 19:50:45 -0300

    helper functions

---
+++ src/shared/utils/function.js
@@ -0,0 +1,21 @@
+/* eslint-disable import/prefer-default-export */
+
+export const throttle = (fn, limit) => {
+  let lastFn;
+  let lastRan;
+  return function newFn(...args) {
+    const context = this;
+    if (!lastRan) {
+      fn.apply(context, args);
+      lastRan = Date.now();
+    } else {
+      clearTimeout(lastFn);
+      lastFn = setTimeout(() => {
+        if ((Date.now() - lastRan) >= limit) {
+          fn.apply(context, args);
+          lastRan = Date.now();
+        }
+      }, limit - (Date.now() - lastRan));
+    }
+  };
+};
