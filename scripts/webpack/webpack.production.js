babfb9a153d8a0ed0a4c48a1214d1b3d7c19bdbf
Carlos Roberto Gomes Junior <carlos.rberto@gmail.com>
2019-03-15 17:28:42 -0300

    webpack publicPath

--- scripts/webpack/webpack.production.js
+++ scripts/webpack/webpack.production.js
@@ -9,6 +9,7 @@ module.exports = merge(common, {
   mode: 'production',
   output: {
     filename: '[name].[hash].js',
+    publicPath: '/parallelogram-generator/',
   },
   optimization: {
     minimizer: [
